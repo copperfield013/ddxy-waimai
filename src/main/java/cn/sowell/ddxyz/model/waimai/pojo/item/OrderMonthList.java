a83c17d53c5ad2313ca1c1ee757532fd90bc3e46
kxli <kxli@e45dfff0-b9ab-ad42-877c-b49451c0a3df>
2017-06-21 08:36:21 +0000

    git-svn-id: http://47.100.187.235:13932/svn/ddxy-waimai@53 e45dfff0-b9ab-ad42-877c-b49451c0a3df

---
+++ src/main/java/cn/sowell/ddxyz/model/waimai/pojo/item/OrderMonthList.java
@@ -0,0 +1,42 @@
+package cn.sowell.ddxyz.model.waimai.pojo.item;
+
+
+public class OrderMonthList {
+	private Integer theYear;
+	private Integer theMonth;
+	private Integer income;
+	private Integer orderCount;
+	private Integer cupCount;
+	
+	public Integer getTheYear() {
+		return theYear;
+	}
+	public void setTheYear(Integer theYear) {
+		this.theYear = theYear;
+	}
+	public Integer getTheMonth() {
+		return theMonth;
+	}
+	public void setTheMonth(Integer theMonth) {
+		this.theMonth = theMonth;
+	}
+	public Integer getIncome() {
+		return income;
+	}
+	public void setIncome(Integer income) {
+		this.income = income;
+	}
+	public Integer getOrderCount() {
+		return orderCount;
+	}
+	public void setOrderCount(Integer orderCount) {
+		this.orderCount = orderCount;
+	}
+	public Integer getCupCount() {
+		return cupCount;
+	}
+	public void setCupCount(Integer cupCount) {
+		this.cupCount = cupCount;
+	}
+
+}
